7d6bf52749b540f3280d07c6c030807662eae839
Samuel França <samuelffn@gmail.com>
2020-07-23 10:15:33 -0300

    Solução para lowercase-initial-letters

--- lowercase-initial-letters/lowercase-initial-letters.js
+++ lowercase-initial-letters/lowercase-initial-letters.js
@@ -1,57 +1,45 @@
-/* const fs = require('fs');
+const fs = require('fs');
 
 function generate(filePath, modelFile) {
-    console.log('>>> Start...');
     
     fs.readFile(filePath, 'utf8', function (err, data) {
+        console.log('Writing file...');
         if (err) {
             return console.log(err);
         }
 
-        let model = '';
+        let newLine = '';
         const lines = data.split('\n');
 
-        console.log(err);
-
         lines.forEach(line => {
-            line = line.substring(0, 1).toLowerCase() + line.substring(1);
-            let attribute = '      ' + line + '\n';
-            model += attribute;
+            for(i = 0; i < line.length; i++) {
+                if (i == 0) newLine += '      ' + line.charAt(0).toLowerCase();
+                else if (i+1 == line.length) newLine += line.charAt(i).toLowerCase() + '\n';
+                else if (verifyUpper(line[i]) && verifyUpper(line[i+1])) newLine += line.charAt(i).toLowerCase();
+                else newLine += line[i];
+            }
         });
 
-        fs.writeFile(modelFile, model, 'utf-8', function (err) {
+        fs.writeFile(modelFile, newLine, 'utf-8', function (err) {
             if (err) return console.log(err);
         });
     });
-    console.log('>>> Stop');
 }
 
 generate(__dirname + '/input.txt', __dirname + '/output.txt');
- */
-
-const name = "SamUel";
-//let letter = name.charAt(0);
-let newName = '';
-
-let beforeLower = false;
-for(i = 0; i < name.length; i++) {
-    if ((verifyUpper(name[i]) && !verifyUpper(name[i+1])) && !beforeLower) {
-        newName += name.charAt(i).toLowerCase();
-        beforeLower = true;
-    } else if ((verifyUpper(name[i]) && !verifyUpper(name[i+1])) && !beforeLower) {
-        newName += name.charAt(i).toLowerCase();
-        beforeLower = true;
-    } {
-        console.log(name[i]);
-        newName += name[i];
-        beforeLower = false;
-    }
-}
-console.log(newName);
 
 function verifyUpper(letter){
-    //console.log(letter);
     return letter ? (letter == letter.toUpperCase()) : false;
 }
 
+/*
+const name = "SamuEl2";
+let newName = '';
 
+for(i = 0; i < name.length; i++) {
+    if (i == 0 || i+1 == name.length) newName += name.charAt(i).toLowerCase();
+    else if (verifyUpper(name[i]) && verifyUpper(name[i+1])) newName += name.charAt(i).toLowerCase();
+    else newName += name[i];
+}
+console.log(newName);
+*/
