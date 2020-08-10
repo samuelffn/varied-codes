9f5a8b6367d6739709665a9c6b33f6a47e82eeb7
Samuel França <samuelffn@gmail.com>
2020-08-10 15:57:39 -0300

    Update model-graphql-generator.js

--- camel-case-letters/model-graphql-generator.js
+++ camel-case-letters/model-graphql-generator.js
@@ -1,13 +1,3 @@
-/**
- * Copyright (c) 2020 Samsung Electronics Co., Ltd.
- * All rights reserved.
- *
- * This software is a confidential and proprietary information of Samsung
- * Electronics, Inc. ("Confidential Information").  You shall not disclose such
- * Confidential Information and shall use it only in accordance with the terms
- * of the license agreement you entered into with Samsung Electronics.
- */
-
 const fs = require('fs');
 
 function generate(filePath, modelFile) {
